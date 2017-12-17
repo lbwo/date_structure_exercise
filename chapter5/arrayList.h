1a41549c2b5d2d38918118a9cb3db84f85dafb6e
J\lbwo001 <290327336@qq.com>
2017-12-18 02:22:32 +0800

    chapter5

--- chapter5/arrayList.h
+++ chapter5/arrayList.h
@@ -37,7 +37,61 @@ class arrayList : public linearList<T>
 
       // additional method
       int capacity() const {return arrayLength;}
+	  //5-7 
+	  //overlord []
+	  T& operator[](int i)
+	  {
+		  if (i >= arrayLength)
+		  {
+			  ostringstream s;
+			  s << "argument number > arraylist length!";
+			  throw illegalParameterValue(s.str());
+		  }
+		  return element[i];
+		}
 
+	  //5-8
+	  //overlord ==
+	  bool operator==(const arrayList<T> a)
+	  {
+		  if (a.listSize != listSize)
+		  {
+			  return false;
+		  }
+		  for (int i = 0; i < listSize; i++)
+		  {
+			  if (element[i] != a[i])
+			  {
+				  return false;
+			  }
+		  }
+		  return true;
+	  }
+
+	  //5-9
+	  //overlord !=
+	  bool operator!=(const arrayList<T> a)
+	  {
+		  return !(*this == a);
+	  }
+
+	  //5-11
+	  void push_back(const T& a);
+
+	  //5-13
+	  void swap(arrayList<T>& a);
+
+	  //5-15
+	  T& set(int index, const T& a);
+
+	  //5-17
+	  void removeRange(int start, int end);
+
+	  //5-23
+	  void leftShift(int i);
+
+	  //5-25
+	  void half();
    protected:
       void checkIndex(int theIndex) const;
             // throw illegalIndex if theIndex invalid
@@ -153,6 +207,101 @@ template <class T>
 ostream& operator<<(ostream& out, const arrayList<T>& x)
    {x.output(out); return out;}
 
+//5-11
+template <class T>
+void arrayList<T>::push_back(const T& a)
+{
+	if (listSize == arrayLength)
+	{
+		changeLength1D(element, arrayLength, 2 * arrayLength);
+		arrayLength *= 2;
+	}
+	element[listSize] = a;
+	listSize++;
+	return;
+}
 
+//5-13
+//交换两个线性表的元素
+template<class T>
+void arrayList<T>::swap(arrayList<T>& a)
+{
+	swap(arrayLength, a.arrayLength);
+	swap(listSize, a.listSize);
+	swap(element, a.element);
+	return;
+}
 
+//5-15
+template<class T>
+T& arrayList<T>::set(int index, const T& a)
+{
+	checkIndex(index);
+	T temp = element[index];
+	element[index] = a;
+	return temp;
+}
+
+//5-17
+template<class T>
+void arrayList<T>::removeRange(int start, int end)
+{
+	if (start <0 || end >listSize)
+	{
+		throw illegalIndex();
+	}
+	if (start > end)
+	{
+		return;
+	}
+	copy(element + end, element + listSize, element + start);
+	int newSize = listSize - end + start;
+	for (int i = newSize; i < listSize; i++)
+	{
+		element[i].~T();
+	}
+	listSize = newSize;
+	return;
+}
+
+//5-23
+//左移i个元素
+template<class T>
+void arrayList<T>::leftShift(int i)
+{
+	if (i < 0)
+	{
+		throw illegalIndex("i must be >= 0");
+	}
+	int  newSize = 0;
+	if (i < listSize)
+	{
+		copy(element + i, element + listSize, element);
+		newSize = listSize - i;
+	}
+	for (int i = newSize; i < listSize; i++)
+	{
+		element[i].~T();
+	}
+	listSize = newSize;
+	return;
+}
+
+//5-25
+//x的元素隔一个删除一个
+template<class T>
+void arrayList<T>::half()
+{
+
+	int newSize = ceil((double)listSize / 2);
+	T* temp = new T[newSize];
+	for (int i=0,j = 0; i < newSize; i++,j=j+2)
+	{
+		temp[i] = element[j];
+	}
+	delete[] element;
+	element = temp;
+	listSize = newSize;
+	return;
+}
 #endif
--- chapter5/源.cpp
+++ chapter5/源.cpp
@@ -61,6 +61,55 @@ int main()
 	//a.trimToSize();
 	//cout << "after:" << a.capacity() << endl;
 
+	//5-7
+	/*arrayList<int> a;
+	a[1] = 1;
+	int b = a[1];
+	cout << a[1]<<b;
+	*/
 
+	//5-8 5-9
+	/*arrayList<int> a, b(20);
+	a.insert(0, 1);
+	b.insert(0, 1);
+	cout << (a == b);
+	cout << (a != b);
+	*/
+
+	//5-11
+	/*arrayList<int> a;
+	a.push_back(1);
+	cout<<a[0];*/
+	
+	//5-15
+	/*arrayList<int> a(10);
+	a.insert(0, 1);
+	
+	cout << a.set(0, 3);
+	cout << a[0];*/
+
+	//5-23
+	/*arrayList<int> a(10);
+	for (int i = 0; i < 5; i++)
+	{
+		a.push_back(i);
+	}
+	a.output(cout);
+	a.leftShift(2);
+	a.output(cout);*/
+
+	//5-25
+	/*arrayList<int> a(7);
+	a.push_back(2);
+	a.push_back(13);
+	a.push_back(4);
+	a.push_back(5);
+	a.push_back(17);
+	a.push_back(8);
+	a.push_back(29);
+	a.output(cout);
+	a.half();
+	cout << endl;
+	a.output(cout);*/
 	return 0;
 }
